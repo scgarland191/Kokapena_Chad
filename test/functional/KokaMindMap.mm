988620f7b40339d08e6cdac5ac7c6e0563f36840
Chad Garland <cgarland@GNVcgarland0FD58.local>
2014-06-11 01:32:18 -0400

    Add MindMaps

---
+++ test/functional/KokaMindMap.mm
@@ -0,0 +1,143 @@
+
+<map version="0.9.0">
+    <node TEXT="Test Kokapena" FOLDED="false">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT="Functional" FOLDED="false" POSITION="right">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="History" FOLDED="false">
+                <edge COLOR="#79a5e5" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Viewing venue list" FOLDED="false">
+                    <edge COLOR="#7296e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Viewing venue properties?" FOLDED="false">
+                    <edge COLOR="#7ab3e7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Offers" FOLDED="false">
+                <edge COLOR="#839de5" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Viewing venue list" FOLDED="false">
+                    <edge COLOR="#869de7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Viewing venue properties" FOLDED="false">
+                    <edge COLOR="#7c8ce5" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="My venues" FOLDED="false">
+                <edge COLOR="#7494e4" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Timewall posts" FOLDED="false">
+                    <edge COLOR="#7a9de5" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Venue checkins" FOLDED="false">
+                    <edge COLOR="#779de2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Deleting venues" FOLDED="false">
+                    <edge COLOR="#6e8fe1" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Viewing venue list" FOLDED="false">
+                    <edge COLOR="#7a99e7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Editing venues" FOLDED="false">
+                    <edge COLOR="#6f93e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Venue offers" FOLDED="false">
+                    <edge COLOR="#7491e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Creating" FOLDED="false">
+                        <edge COLOR="#7988e3" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Deleting" FOLDED="false">
+                        <edge COLOR="#788ae2" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+            <node TEXT="Existing venues" FOLDED="false">
+                <edge COLOR="#79aae5" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Check in" FOLDED="false">
+                    <edge COLOR="#71afe2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Previous name" FOLDED="false">
+                        <edge COLOR="#79b1e3" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Rolling" FOLDED="false">
+                        <edge COLOR="#78b0e2" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Posting" FOLDED="false">
+                        <edge COLOR="#79bee1" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Commenting" FOLDED="false">
+                        <edge COLOR="#7ab1e2" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+                <node TEXT="Lurk" FOLDED="false">
+                    <edge COLOR="#7caae7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Previous Name" FOLDED="false">
+                        <edge COLOR="#7ea3e6" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Rolling" FOLDED="false">
+                        <edge COLOR="#749ae4" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Posting" FOLDED="false">
+                        <edge COLOR="#7ba9e7" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Commenting" FOLDED="false">
+                        <edge COLOR="#74a0e6" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+            <node TEXT="Adding a venue" FOLDED="false">
+                <edge COLOR="#81b3e5" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Set venue properties" FOLDED="false">
+                    <edge COLOR="#7fade4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Set venue location" FOLDED="false">
+                    <edge COLOR="#86c2e7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Set venue photo" FOLDED="false">
+                    <edge COLOR="#8ab5e5" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Locations" FOLDED="false">
+                <edge COLOR="#80a0e6" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="My location" FOLDED="false">
+                    <edge COLOR="#88a1e7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Venue locations" FOLDED="false">
+                    <edge COLOR="#87a9e6" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
---
+++ test/functional/KokaXmindMap.mm
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?><map version="0.8.1"><node CREATED="1402464628623" ID="0aol7vohiauutrh9mqdhatmjh9" MODIFIED="1402464628623" TEXT="Test Kokapena"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628623" ID="6d3sj8779g6tn72fmt5djaaqjn" MODIFIED="1402464628623" POSITION="right" TEXT="Functional"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628623" ID="0ejknk2hv6s21fnf2hv5i0vm04" MODIFIED="1402464628623" TEXT="History"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628623" ID="4mi5r5vmpa4vf4nif0k5eqljv9" MODIFIED="1402464628623" TEXT="Viewing venue list"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628623" ID="618lugtubbiotgfej9341ov7h4" MODIFIED="1402464628623" TEXT="Viewing venue properties?"><font NAME="SansSerif" SIZE="10"/></node></node><node CREATED="1402464628623" ID="03ajkhbfo1udn0im7th4misakj" MODIFIED="1402464628623" TEXT="Offers"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628623" ID="2vav6f2lbru83dus3ujlar3rct" MODIFIED="1402464628623" TEXT="Viewing venue list"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628623" ID="7ht7hnaovr47m4seeic8cum4gk" MODIFIED="1402464628624" TEXT="Viewing venue properties"><font NAME="SansSerif" SIZE="10"/></node></node><node CREATED="1402464628624" ID="36udle15l4hf7cmvuakattf22a" MODIFIED="1402464628624" TEXT="My venues"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628624" ID="3v8tb1u7omtkq01bpj2igqqe9a" MODIFIED="1402464628624" TEXT="Timewall posts"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="4iigbcocvoi344r85gk7anrgjr" MODIFIED="1402464628624" TEXT="Venue checkins"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="09k8sgn34o3k4kafieru2m8jj9" MODIFIED="1402464628624" TEXT="Deleting venues"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="70rq20tpkmtblirtvos224itv1" MODIFIED="1402464628624" TEXT="Viewing venue list"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="1b81hn30t1ls581vptrr430ji0" MODIFIED="1402464628624" TEXT="Editing venues"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="4edv00ha21jrmmqp02efl1lrb5" MODIFIED="1402464628624" TEXT="Venue offers"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628624" ID="6396lgg7lo72mlfrgpr2knad1c" MODIFIED="1402464628624" TEXT="Creating"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="0ollcp7cf9eug59kv745jhp5nv" MODIFIED="1402464628624" TEXT="Deleting"><font NAME="SansSerif" SIZE="10"/></node></node></node><node CREATED="1402464628624" ID="7eitjttd2r286auebjdqleg3pp" MODIFIED="1402464628624" TEXT="Existing venues"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628624" ID="1ritlhov20ur7mf2kptued5dtp" MODIFIED="1402464628624" TEXT="Check in"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628624" ID="09gkt45o4j93gar4q8t66a2upi" MODIFIED="1402464628624" TEXT="Previous name"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628624" ID="7lipq91g27ffnmkuva1pliptjk" MODIFIED="1402464628624" TEXT="Rolling"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="5gngqo6us09g7rf0bd4p82kv14" MODIFIED="1402464628625" TEXT="Posting"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="429o1mmtv1o0r654shfbqp5gh0" MODIFIED="1402464628625" TEXT="Commenting"><font NAME="SansSerif" SIZE="10"/></node></node><node CREATED="1402464628625" ID="621d8nn779da5n1b35e4rovg0d" MODIFIED="1402464628625" TEXT="Lurk"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628625" ID="2vd4ropmavi429a2gvhucvjbje" MODIFIED="1402464628625" TEXT="Previous Name"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="2q60qid7mf0pvrsbtk9316shv4" MODIFIED="1402464628625" TEXT="Rolling"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="29dgbb0o8loig8fd759jter920" MODIFIED="1402464628625" TEXT="Posting"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="6ml5dc06frivtsbqkuamogiq2d" MODIFIED="1402464628625" TEXT="Commenting"><font NAME="SansSerif" SIZE="10"/></node></node></node><node CREATED="1402464628625" ID="1ugmqa9hn4fmrfsjuo4rsqe02j" MODIFIED="1402464628625" TEXT="Adding a venue"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628625" ID="6jh1u2npscdmiupjr53c3l6fta" MODIFIED="1402464628625" TEXT="Set venue properties"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="3rbcopo0mhmkfvvliqger1j473" MODIFIED="1402464628625" TEXT="Set venue location"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628625" ID="1e7tfpa84e6gated7e481g10ap" MODIFIED="1402464628625" TEXT="Set venue photo"><font NAME="SansSerif" SIZE="10"/></node></node><node CREATED="1402464628625" ID="3rru43ejcdvndcrjg7ai9futam" MODIFIED="1402464628625" TEXT="Locations"><font NAME="SansSerif" SIZE="10"/><node CREATED="1402464628625" ID="04e3lpq7408bk6m8n2ksk9hb2f" MODIFIED="1402464628625" TEXT="My location"><font NAME="SansSerif" SIZE="10"/></node><node CREATED="1402464628626" ID="7t38s2e8gdhp2ev5r9d4nf1s0p" MODIFIED="1402464628626" TEXT="Venue locations"><font NAME="SansSerif" SIZE="10"/></node></node></node><node CREATED="1402464628626" ID="5hnue7itvckescr9231gqob9j6" MODIFIED="1402464628626" POSITION="right" TEXT="Sprints"><node CREATED="1402464628626" ID="79gedlms7pofsrcfia8e9p3ud7" MODIFIED="1402464628626" TEXT="Sprint1"/><node CREATED="1402464628626" ID="7t02r7se0kj3lilfl7ctm6o1ia" MODIFIED="1402464628626" TEXT="Sprint2"/><node CREATED="1402464628626" ID="6q7mi87brvu9cijc4rfr6t1s0k" MODIFIED="1402464628626" TEXT="Sprint3"><node CREATED="1402464628626" ID="6jkd2cq1vltpqm26rkism27d85" MODIFIED="1402464628626" TEXT="Charter1"><hook NAME="accessories/plugins/NodeNote.properties"><text>Objective: Verify users can view offers, posts, comments and the number of comments per post.
+
+User Stories: [KOK-47], [KOK-222], [KOK-263]
+
+Time: 25m
+
+Platforms: iOS 7 iPhone
+
+Start: 6/11/14 ??a.m.
+
+Notes:
+Existing offers can be seen for venues.
+Offer properties are visible.
+Posts can be seen for venues.
+Comments can be seen on posts.
+The number of comments are displayed on posts.
+The number of comments is accurate.</text></hook><node CREATED="1402464628626" ID="7s6df7h1mgvj89v9ua1v7nb524" MODIFIED="1402464628626" TEXT="Subtopic 1"/></node><node CREATED="1402464628626" ID="5p7n01f2e6r2e84i18g5muu4el" MODIFIED="1402464628626" TEXT="Charter2"><hook NAME="accessories/plugins/NodeNote.properties"><text>Objective: Verify admins can add venue properties, upload venue images, and make venue offers.
+
+User Stories: [KOK-34], [KOK-37], [KOK-46]
+
+Time: 25m
+
+Platforms: iOS 7 iPhone
+
+Start: 6/12/14 12:30 a.m.
+
+Notes:
+Users should be able to add venue properties to their venues.
+Users shouldn't be able to alter venue properties to other venues.
+Users should be able to upload venue images to their venues.
+Users shouldn't be able to upload venue images to other venues.
+Users should be able to make offers at their venues.
+Users shouldn't be able to make offers at other venues.
+
+Results:
++	Users can only admin their own venues.
+-	Users can add venues on top of others and make fraudulent offers.
+-	Offers remain in the offers page after offer deletion.
+-	Offers remain in the offers page after venue deletion.
+-	App crashes when different time options are selected for the timewall data in the absence of posts.</text></hook></node></node><node CREATED="1402464628626" ID="4hda729ir4vq6n2npc90ang39f" MODIFIED="1402464628626" TEXT="Sprint4"/></node></node></map>
